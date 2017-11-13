fb56278209ec7eb88874c6bbf75271236c8d5a0f
Carleton Coffrin <carleton@coffr.in>
2017-11-13 16:47:03 -0700

    Code Refractor (#6)
    * adding GMLFormulation and GMLMethod types
    
    * adding data_info helper
    
    * tweaking data types
    
    * adding GMLFormulation and GMLMethod types
    
    * adding data_info helper
    
    * tweaking data types
    
    * updating gibbs_sampler to sample interface
    
    * adding types to sample
    
    * adding mvt test case
    
    * fix parameters
    
    * updating regression tests
    
    * removing legacy inverse_ising_archive function

--- src/GraphicalModelLearning.jl
+++ src/GraphicalModelLearning.jl
@@ -2,86 +2,187 @@ isdefined(Base, :__precompile__) && __precompile__()
 
 module GraphicalModelLearning
 
-export inverse_ising, gibbs_sampler
+export learn, inverse_ising
+
+export GMLFormulation, RISE, logRISE, RPLE
+export GMLMethod, NLP
 
 using JuMP
+using MathProgBase # for solver type
 using Ipopt
 
-# formulations: :RISE, :logRISE, :RPLE
+using Compat # used for julia v0.5 abstract types
 
-function inverse_ising(samples_histo; method::Symbol=:logRISE, regularizing_value::Float64=0.8, symmetrization::Symbol=:Yes)
-    (num_conf, num_row) = size(samples_histo)
-    num_spins           = num_row - 1
+@compat abstract type GMLFormulation end
+
+type RISE <: GMLFormulation
+    regularizer::Real
+    symmetrization::Bool
+end
+# default values
+RISE() = RISE(0.4, true)
+
+type logRISE <: GMLFormulation
+    regularizer::Real
+    symmetrization::Bool
+end
+# default values
+logRISE() = logRISE(0.8, true)
+
+type RPLE <: GMLFormulation
+    regularizer::Real
+    symmetrization::Bool
+end
+# default values
+RPLE() = RPLE(0.2, true)
+
+
+@compat abstract type GMLMethod end
+
+type NLP <: GMLMethod
+    solver::MathProgBase.AbstractMathProgSolver
+end
+# default values
+NLP() = NLP(IpoptSolver(print_level=0))
+
+
+# default settings
+learn{T <: Real}(samples::Array{T,2}) = learn(samples, RISE(), NLP())
+learn{T <: Real, S <: GMLFormulation}(samples::Array{T,2}, formulation::S) = learn(samples, formulation, NLP())
+
+
+function data_info{T <: Real}(samples::Array{T,2})
+    (num_conf, num_row) = size(samples)
+    num_spins = num_row - 1
+    num_samples = sum(samples[1:num_conf,1])
+    return num_conf, num_spins, num_samples
+end
+
+
+function learn{T <: Real}(samples::Array{T,2}, formulation::RISE, method::NLP)
+    num_conf, num_spins, num_samples = data_info(samples)
+
+    lambda = formulation.regularizer*sqrt(log((num_spins^2)/0.05)/num_samples)
 
     reconstruction = Array{Float64}(num_spins, num_spins)
 
-    #Extraction of the total number of samples contained in the histogram.
-    num_samples = sum(samples_histo[1:num_conf,1])
-
-    #Initialization of the regularizing parameter from regularization coefficient. Declaration of the RPLE and RISE objective funcions.
-    lambda           = regularizing_value*sqrt(log((num_spins^2)/0.05)/num_samples)
-    RISEobjective(h) = exp(-h)
-    RPLEobjective(h) = log(1 + exp(-2h))
-
-    #Loop for the reconstruction of couplings around "current_spin"
     for current_spin = 1:num_spins
-        #Printing out progress
-        #println("Reconstructing the parameters adjacent to node ", current_spin);
+        nodal_stat  = [ samples[k, 1 + current_spin] * (i == current_spin ? 1 : samples[k, 1 + i]) for k=1:num_conf , i=1:num_spins]
 
-        #Construction of the statistics around node "current_spin" in the histogram format.
-        #An element (k,i) reads "configuration[k,current_spin] * configuration[k,i]" if i != current_spin and "configuration[k,current_spin]" otherwise.
-        nodal_stat  = [ samples_histo[k, 1 + current_spin] * (i == current_spin ? 1 : samples_histo[k, 1 + i]) for k=1:num_conf , i=1:num_spins]
+        m = Model(solver = method.solver)
 
-        #Declaration in JuMP of the optimization model "m" and the convex solver. Here the convex solver is choosen to be Ipopt.
-        #The tolerance tol is chosen based on experiments in the paper, remove for using a default tolerance of the solver, e.g. solver = IpoptSolver()
-        #The option print_level=0 disables the output of the Ipopt solver, remove for reading a default detailed information on the convergence, e.g. solver = IpoptSolver()
-        m = Model(solver = IpoptSolver(tol=1e-12, print_level=0))
-
-        #Initialization of the loss function for JuMP. RISE is choosen by default unless the user specifies RPLE in the arguments.
-        JuMP.register(m, :IIPobjective, 1, (method == :RPLE ? RPLEobjective : RISEobjective), autodiff=true)
-
-        #Declaration in JuMP of "x", the array of variables for couplings and magnetic fields and "z", the array of slack variables for the l1 norm.
-        #The magnetic field variable is x[current_spin] and the coupling variables are x[i] for i!= current_spin. The slack variable z[current_spin] is uneccessary.
         @variable(m, x[1:num_spins])
         @variable(m, z[1:num_spins])
 
-        #Declaration in JuMP of the objective function: (log(RISE) + l1, RISE + l1, RPLE + l1). RISE is choosen by default unless the user specifies RPLE or logRISE in the arguments.
-        if method == :logRISE
-            @NLobjective(m, Min,
-                log(sum((samples_histo[k,1]/num_samples)* IIPobjective(sum(x[i]*nodal_stat[k,i] for i=1:num_spins)) for k=1:num_conf)) +
-                lambda*sum(z[j] for j=1:num_spins if current_spin!=j)
-                )
-        else
-            @NLobjective(m, Min,
-                sum((samples_histo[k,1]/num_samples)* IIPobjective(sum(x[i]*nodal_stat[k,i] for i=1:num_spins)) for k=1:num_conf) +
-                lambda*sum(z[j] for j=1:num_spins if current_spin!=j)
-                )
-        end
+        @NLobjective(m, Min,
+            sum((samples[k,1]/num_samples)*exp(-sum(x[i]*nodal_stat[k,i] for i=1:num_spins)) for k=1:num_conf) +
+            lambda*sum(z[j] for j=1:num_spins if current_spin!=j)
+        )
 
-        #Declaration in JuMP of slack constraints for the l1 penalty.
         for j in 1:num_spins
             @constraint(m, z[j] >=  x[j]) #z_plus
             @constraint(m, z[j] >= -x[j]) #z_minus
         end
 
-        #Lauching convex optimization, printing results and updating the matrix of reconstructed parameters accordingly.
         status = solve(m)
-        #println(current_spin, " = ", getvalue(x))
+        @assert status == :Optimal
         reconstruction[current_spin,1:num_spins] = deepcopy(getvalue(x))
-
     end
 
-    #symmetrization of the couplings. No symmetrization is choosen by defaut unless the user specifies "Y" in the arguments.
-    if symmetrization == "Y"
+    if formulation.symmetrization
         reconstruction = 0.5*(reconstruction + transpose(reconstruction))
     end
 
-    return(reconstruction)
+    return reconstruction
+end
+
+
+function learn{T <: Real}(samples::Array{T,2}, formulation::logRISE, method::NLP)
+    num_conf, num_spins, num_samples = data_info(samples)
+
+    lambda = formulation.regularizer*sqrt(log((num_spins^2)/0.05)/num_samples)
+
+    reconstruction = Array{Float64}(num_spins, num_spins)
+
+    for current_spin = 1:num_spins
+        nodal_stat  = [ samples[k, 1 + current_spin] * (i == current_spin ? 1 : samples[k, 1 + i]) for k=1:num_conf , i=1:num_spins]
+
+        m = Model(solver = method.solver)
+
+        @variable(m, x[1:num_spins])
+        @variable(m, z[1:num_spins])
+
+        @NLobjective(m, Min,
+            log(sum((samples[k,1]/num_samples)*exp(-sum(x[i]*nodal_stat[k,i] for i=1:num_spins)) for k=1:num_conf)) +
+            lambda*sum(z[j] for j=1:num_spins if current_spin!=j)
+        )
+
+        for j in 1:num_spins
+            @constraint(m, z[j] >=  x[j]) #z_plus
+            @constraint(m, z[j] >= -x[j]) #z_minus
+        end
+
+        status = solve(m)
+        @assert status == :Optimal
+        reconstruction[current_spin,1:num_spins] = deepcopy(getvalue(x))
+    end
+
+    if formulation.symmetrization
+        reconstruction = 0.5*(reconstruction + transpose(reconstruction))
+    end
+
+    return reconstruction
+end
+
+
+function learn{T <: Real}(samples::Array{T,2}, formulation::RPLE, method::NLP)
+    num_conf, num_spins, num_samples = data_info(samples)
+
+    lambda = formulation.regularizer*sqrt(log((num_spins^2)/0.05)/num_samples)
+
+    reconstruction = Array{Float64}(num_spins, num_spins)
+
+    for current_spin = 1:num_spins
+        nodal_stat  = [ samples[k, 1 + current_spin] * (i == current_spin ? 1 : samples[k, 1 + i]) for k=1:num_conf , i=1:num_spins]
+
+        m = Model(solver = method.solver)
+
+        @variable(m, x[1:num_spins])
+        @variable(m, z[1:num_spins])
+
+        @NLobjective(m, Min,
+            sum((samples[k,1]/num_samples)*log(1 + exp(-2*sum(x[i]*nodal_stat[k,i] for i=1:num_spins))) for k=1:num_conf) +
+            lambda*sum(z[j] for j=1:num_spins if current_spin!=j)
+        )
+
+        for j in 1:num_spins
+            @constraint(m, z[j] >=  x[j]) #z_plus
+            @constraint(m, z[j] >= -x[j]) #z_minus
+        end
+
+        status = solve(m)
+        @assert status == :Optimal
+        reconstruction[current_spin,1:num_spins] = deepcopy(getvalue(x))
+    end
+
+    if formulation.symmetrization
+        reconstruction = 0.5*(reconstruction + transpose(reconstruction))
+    end
+
+    return reconstruction
 end
 
 
 
+export sample
+
+export GMSampler, Gibbs
+
 using StatsBase
+
+@compat abstract type GMSampler end
+
+type Gibbs <: GMSampler end
 
 function int_to_spin(int_representation, spin_number)
   spin = 2*digits(int_representation, 2, spin_number)-1
@@ -94,14 +195,14 @@ function weigh_proba(int_representation, adj, prior)
   return exp(((0.5) * current_spin' * adj * current_spin + prior * current_spin)[1])
 end
 
-function sample_generation(sample_number, adj, prior)
+function sample_generation{T <: Real}(sample_number::Integer, adj::Array{T,2}, prior)
   spin_number   = size(adj,1)
   config_number = 2^spin_number
 
   items   = [i for i in 0:(config_number-1)]
   weights = [weigh_proba(i, adj, prior) for i in (0:config_number-1)]
 
-  raw_sample = sample(items, StatsBase.Weights(weights), sample_number)
+  raw_sample = StatsBase.sample(items, StatsBase.Weights(weights), sample_number)
   raw_binning= countmap(raw_sample)
 
   spin_sample = [ vcat(raw_binning[i], int_to_spin(i, spin_number)) for i in keys(raw_binning)]
@@ -109,7 +210,8 @@ function sample_generation(sample_number, adj, prior)
   return spin_sample
 end
 
-function gibbs_sampler(adjacency_matrix, number_sample::Int64)
+sample{T <: Real}(adjacency_matrix::Array{T,2}, number_sample::Integer) = sample(adjacency_matrix, number_sample, Gibbs())
+function sample{T <: Real}(adjacency_matrix::Array{T,2}, number_sample::Integer, sampler::Gibbs)
     prior_vector = transpose(diag(adjacency_matrix)) #priors, or magnetic fields part
 
     #Generation of samples
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using GraphicalModelLearning
-
+using Ipopt
 using Base.Test
 
 include("common.jl")
@@ -8,7 +8,7 @@ include("common.jl")
 @testset "gibbs sampler" begin
     for (name, gm) in gms
         srand(0) # fix random number generator
-        samples = gibbs_sampler(gm, gibbs_test_samples)
+        samples = sample(gm, gibbs_test_samples)
         base_samples = readcsv("data/$(name)_samples.csv")
         @test isapprox(samples, base_samples)
     end
@@ -16,43 +16,67 @@ end
 
 
 @testset "inverse ising formulations" begin
-    for formulation in formulations
-        @testset "  $(formulation)" begin
+
+    for (form_name, formulation) in formulations
+        @testset "  $(form_name)" begin
             for (name, gm) in gms
                 samples = readcsv("data/$(name)_samples.csv")
                 srand(0) # fix random number generator
-                learned_gm = inverse_ising(samples, method=formulation)
-                base_learned_gm = readcsv("data/$(name)_$(formulation)_learned.csv")
+                #learned_gm = inverse_ising(samples, method=formulation)
+                learned_gm = learn(samples, formulation)
+                base_learned_gm = readcsv("data/$(name)_$(form_name)_learned.csv")
+                #println(maximum(abs.(learned_gm - base_learned_gm)))
                 @test isapprox(learned_gm, base_learned_gm)
             end
         end
     end
+
+
+    samples = readcsv("data/mvt_samples.csv")
+
+    rand(0) # fix random number generator
+    learned_gm_rise = learn(samples, RISE(0.2, false), NLP(IpoptSolver(print_level=0)))
+    base_learned_gm = readcsv("data/mvt_RISE_learned.csv")
+    #println(abs.(learned_gm_rise - base_learned_gm))
+    @test isapprox(learned_gm_rise, base_learned_gm)
+
+    srand(0) # fix random number generator
+    learned_gm_logrise = learn(samples, logRISE(0.2, false), NLP(IpoptSolver(print_level=0)))
+    base_learned_gm = readcsv("data/mvt_logRISE_learned.csv")
+    #println(abs.(learned_gm_logrise - base_learned_gm))
+    @test isapprox(learned_gm_logrise, base_learned_gm)
+
+    srand(0) # fix random number generator
+    learned_gm_rple = learn(samples, RPLE(0.2, false), NLP(IpoptSolver(print_level=0)))
+    base_learned_gm = readcsv("data/mvt_RPLE_learned.csv")
+    #println(abs.(learned_gm_rple - base_learned_gm))
+    @test isapprox(learned_gm_rple, base_learned_gm)
 end
 
 
 type AccuracyTest
-   formulation::Symbol
+   formulation::GMLFormulation
    samples::Int
    threshold::Float64
 end
 
 accuracy_tests = [
-    AccuracyTest(:RISE,     1000, 0.15)
-    AccuracyTest(:logRISE,  1000, 0.15)
-    AccuracyTest(:RPLE,     1000, 0.15)
-    AccuracyTest(:RISE,    10000, 0.05)
-    AccuracyTest(:logRISE, 10000, 0.05)
-    AccuracyTest(:RPLE,    10000, 0.05)
+    AccuracyTest(RISE(),     1000, 0.15)
+    AccuracyTest(logRISE(),  1000, 0.15)
+    AccuracyTest(RPLE(),     1000, 0.15)
+    AccuracyTest(RISE(),    10000, 0.05)
+    AccuracyTest(logRISE(), 10000, 0.05)
+    AccuracyTest(RPLE(),    10000, 0.05)
 ]
 
 srand(0) # fix random number generator
 @testset "learned model accuracy" begin
     for act in accuracy_tests
-        test_name = eval("$(act.formulation) $(act.samples) $(act.threshold)")
         @testset "  $(act.formulation) $(act.samples) $(act.threshold)" begin
             for (gm_name, gm) in gms
-                sample_histo = gibbs_sampler(gm, act.samples)
-                learned_gm = inverse_ising(sample_histo, method=act.formulation)
+                sample_histo = sample(gm, act.samples)
+                #learned_gm = inverse_ising(sample_histo, method=act.formulation)
+                learned_gm = learn(sample_histo, act.formulation)
                 max_error = maximum(abs.(gm - learned_gm))
                 @test max_error <= act.threshold
             end
