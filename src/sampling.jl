8305919051ef521908119a694055e96ca0a22ef6
carleton <cjc@lanl.gov>
2017-11-18 15:48:29 -0700

    improve memory allocation when enumerating states

--- src/sampling.jl
+++ src/sampling.jl
@@ -15,9 +15,19 @@ end
 
 function weigh_proba{T <: Real}(int_representation::Int, adj::Array{T,2}, prior::Array{T,1})
     spin_number  = size(adj,1)
-    current_spins = int_to_spin(int_representation, spin_number)
-    return exp(((0.5) * current_spins' * adj * current_spins + prior' * current_spins)[1])
+    spins = int_to_spin(int_representation, spin_number)
+    return exp(((0.5) * spins' * adj * spins + prior' * spins)[1])
 end
+
+
+bool_to_spin(bool::Int) = 2*bool-1
+
+function weigh_proba{T <: Real}(int_representation::Int, adj::Array{T,2}, prior::Array{T,1}, assignment_tmp::Array{Int,1})
+    digits!(assignment_tmp, int_representation, 2)
+    bool_to_spin.(assignment_tmp)
+    return exp(((0.5) * assignment_tmp' * adj * assignment_tmp + prior' * assignment_tmp)[1])
+end
+
 
 function sample_generation{T <: Real}(samples_per_bin::Integer, adj::Array{T,2}, prior::Array{T,1}, bins::Int)
     @assert bins >= 1
@@ -25,9 +35,10 @@ function sample_generation{T <: Real}(samples_per_bin::Integer, adj::Array{T,2},
     spin_number   = size(adj,1)
     config_number = 2^spin_number
 
-    items   = [i for i in 0:(config_number-1)]
-    weights = [weigh_proba(i, adj, prior) for i in (0:config_number-1)]
+    assignment_tmp = [0 for i in 1:spin_number] # pre allocate assignment memory
+    weights = [weigh_proba(i, adj, prior, assignment_tmp) for i in (0:config_number-1)]
 
+    items = [i for i in 0:(config_number-1)]
     raw_sample = StatsBase.sample(items, StatsBase.Weights(weights), samples_per_bin*bins, ordered=false)
     raw_sample_bins = reshape(raw_sample, bins, samples_per_bin)
 
