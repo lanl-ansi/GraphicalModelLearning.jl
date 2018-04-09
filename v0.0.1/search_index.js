b1773b93a4371e25754965793c786091ca73582f
autodocs <autodocs>
2018-04-09 20:45:27 +0000

    build based on aeb8bac

---
+++ v0.0.1/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#GraphicalModelLearning.jl-Documentation-1",
+    "page": "Home",
+    "title": "GraphicalModelLearning.jl Documentation",
+    "category": "section",
+    "text": "CurrentModule = GraphicalModelLearning"
+},
+
+{
+    "location": "index.html#Overview-1",
+    "page": "Home",
+    "title": "Overview",
+    "category": "section",
+    "text": "GraphicalModelLearning.jl is a Julia package containing algorithms for learning graphical models."
+},
+
+{
+    "location": "index.html#Installation-and-Testing-1",
+    "page": "Home",
+    "title": "Installation and Testing",
+    "category": "section",
+    "text": "To install the package use,Pkg.add(\"GraphicalModelLearning\")To test that the package is working correctly run,Pkg.test(\"GraphicalModelLearning\")"
+},
+
+{
+    "location": "library.html#",
+    "page": "Library",
+    "title": "Library",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "library.html#GraphicalModelLearning.jl-Library-1",
+    "page": "Library",
+    "title": "GraphicalModelLearning.jl Library",
+    "category": "section",
+    "text": "Modules = [GraphicalModelLearning]"
+},
+
+]}
